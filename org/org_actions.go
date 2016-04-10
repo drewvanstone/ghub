8c181a994496731fd7277fb78211af0f9ab3defd
Drew Flower <drewvanstone@gmail.com>
2016-04-11 07:19:44 +1000

    Add get org members, teams, and hooks
    Clean up error handling a little bit. Moved the gh.Client connection
    to an Init() function so it runs only once.
    
    Add three new commands:
    
    ./ghub get org members [ORG]: retrieves all the members of an org.
    ./ghub get org teams [ORG]: retrieves all teams of an org.
    ./ghub get org hooks [ORG]: retrieves all hooks of an org.
    
    Rename 'client' package to 'gh'.

--- org/org_actions.go
+++ org/org_actions.go
@@ -3,37 +3,73 @@ package org
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
-	"github.com/ghub/client"
+	"github.com/ghub/gh"
 	"github.com/ghub/util"
 	"github.com/google/go-github/github"
-	"log"
 )
 
+// Return a single github.Organization struct
 func getOrg(c *cli.Context) {
-	if len(c.Args()) < 1 {
-		log.Fatal("Some usage")
-	}
 	org := c.Args().First()
+	organization, _, err := gh.Client.Organizations.Get(org)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	gh := client.GitHub()
-	o, _, _ := gh.Organizations.Get(org)
-
-	util.PrintJson(*o)
+	util.PrintJson(organization)
 }
 
-func getReposInOrg(c *cli.Context) {
-	if len(c.Args()) < 1 {
-		log.Fatal("Usage: ghub get org {org}")
-	}
-
+func getOrgRepos(c *cli.Context) {
 	org := c.Args().First()
 	opts := &github.RepositoryListByOrgOptions{
 		ListOptions: github.ListOptions{PerPage: 30},
 	}
-
-	gh := client.GitHub()
-	repos, _, _ := gh.Repositories.ListByOrg(org, opts)
-	for _, repo := range repos {
-		fmt.Println(*repo.Name)
+	repos, _, err := gh.Client.Repositories.ListByOrg(org, opts)
+	if err != nil {
+		fmt.Println(err)
 	}
+
+	util.PrintJson(repos)
+}
+
+func getOrgTeams(c *cli.Context) {
+	org := c.Args().First()
+	opts := &github.ListOptions{PerPage: 30}
+	teams, _, err := gh.Client.Organizations.ListTeams(org, opts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if len(teams) == 0 {
+		fmt.Println("No teams found")
+	}
+
+	util.PrintJson(teams)
+}
+
+func getOrgMembers(c *cli.Context) {
+	org := c.Args().First()
+	opts := &github.ListMembersOptions{
+		ListOptions: github.ListOptions{PerPage: 30},
+	}
+	members, _, err := gh.Client.Organizations.ListMembers(org, opts)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	util.PrintJson(members)
+}
+
+func getOrgHooks(c *cli.Context) {
+	org := c.Args().First()
+	opts := &github.ListOptions{PerPage: 30}
+
+	hooks, _, err := gh.Client.Organizations.ListHooks(org, opts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if len(hooks) == 0 {
+		fmt.Println("No hooks found")
+	}
+
+	util.PrintJson(hooks)
 }
--- util/helpers.go
+++ util/helpers.go
@@ -1,8 +1,26 @@
 package util
 
 import (
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
 	"strings"
 )
+
+func Check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Helper function to check that there are at least as many
+// arguments as required, and if not, display subcommand help.
+func CheckCommandArgs(minArgs int, c *cli.Context) {
+	if len(c.Args()) < minArgs {
+		cli.ShowSubcommandHelp(c)
+		os.Exit(1)
+	}
+}
 
 func SplitOrgRepoName(orgrepo string) (string, string) {
 	parts := strings.Split(orgrepo, "/")
