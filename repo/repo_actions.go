25086a12a94a2007a0f43573789493b095560d58
Drew Flower <drewvanstone@gmail.com>
2016-04-17 08:55:52 +1000

    Add error handling for splitting org/repo

--- repo/repo_actions.go
+++ repo/repo_actions.go
@@ -10,10 +10,10 @@ import (
 )
 
 func getRepo(c *cli.Context) {
-	if len(c.Args()) < 1 {
-		log.Fatal("Usage: ghub org/repo")
+	o, r, err := util.SplitOrgRepoName(c.Args().Get(0))
+	if err != nil {
+		log.Fatal(err)
 	}
-	o, r := util.SplitOrgRepoName(c.Args().Get(0))
 	repo, _, err := gh.Client.Repositories.Get(o, r)
 	if err != nil {
 		log.Fatal(err)
@@ -23,15 +23,13 @@ func getRepo(c *cli.Context) {
 }
 
 func getRepoIssues(c *cli.Context) {
-	if len(c.Args()) < 1 {
-		log.Fatal("Usage: ghub owner/repo")
+	owner, repo, err := util.SplitOrgRepoName(c.Args().Get(0))
+	if err != nil {
+		log.Fatal(err)
 	}
-	owner, repo := util.SplitOrgRepoName(c.Args().Get(0))
-
 	opts := &github.IssueListByRepoOptions{
 		ListOptions: github.ListOptions{PerPage: 30},
 	}
-
 	issues, _, err := gh.Client.Issues.ListByRepo(owner, repo, opts)
 	if err != nil {
 		fmt.Println(err)
@@ -44,15 +42,13 @@ func getRepoIssues(c *cli.Context) {
 }
 
 func getRepoPullRequests(c *cli.Context) {
-	if len(c.Args()) < 1 {
-		log.Fatal("Usage: ghub owner/repo")
+	owner, repo, err := util.SplitOrgRepoName(c.Args().Get(0))
+	if err != nil {
+		log.Fatal(err)
 	}
-	owner, repo := util.SplitOrgRepoName(c.Args().Get(0))
-
 	opts := &github.PullRequestListOptions{
 		ListOptions: github.ListOptions{PerPage: 30},
 	}
-
 	prs, _, err := gh.Client.PullRequests.List(owner, repo, opts)
 	if err != nil {
 		fmt.Println(err)
--- util/helpers.go
+++ util/helpers.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"errors"
 	"github.com/codegangsta/cli"
 	"log"
 	"os"
@@ -22,10 +23,12 @@ func CheckCommandArgs(minArgs int, c *cli.Context) {
 	}
 }
 
-func SplitOrgRepoName(orgrepo string) (string, string) {
+func SplitOrgRepoName(orgrepo string) (string, string, error) {
 	parts := strings.Split(orgrepo, "/")
 	if len(parts) != 2 {
-		println("Repository must be in the form: org/reponame (ie. drewvanstone/ghub), got ", orgrepo)
+		return "", "", errors.New("Failed to split org/repo")
+		os.Exit(1)
+		//log.Fatal("Repository must be in the form: org/reponame (ie. drewvanstone/ghub), got ", orgrepo)
 	}
-	return parts[0], parts[1]
+	return parts[0], parts[1], nil
 }
